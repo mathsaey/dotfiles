630b90922375fa751485db36d271f4325226299c
Mathijs Saey <mathsaey@vub.ac.be>
2017-02-06 14:47:47 +0100

    Added a function to allow iterm to use tmux integration

--- util.sh
+++ util.sh
@@ -3,6 +3,12 @@ function smux {
   ssh $1 -t mux $USER
 }
 
+function imux {
+  # Attach to a tmuxinator session with name $USER over ssh
+  # The session should have already started.
+  ssh $1 -t tmux -CC attach -t $USER
+}
+
 function highlight-snippet {
   # Highlight the current contents of the clipboard
   pbpaste | highlight -O rtf -S $1 -s seashell -K 12 -k menlo --no-trailing-nl | pbcopy
