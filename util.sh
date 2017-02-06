5c628f95e9a33750cf2046621ea913046fe3e0c4
Mathijs Saey <mathsaey@vub.ac.be>
2017-02-06 14:19:41 +0100

    Modified smux comment to use tmux directly

--- util.sh
+++ util.sh
@@ -1,7 +1,9 @@
 function smux {
-  ssh $1 -t mux $USER
+  # Attach to a tmux session with name $USER over ssh
+  ssh $1 -t tmux attach -t $USER
 }
 
 function highlight-snippet {
+  # Highlight the current contents of the clipboard
   pbpaste | highlight -O rtf -S $1 -s seashell -K 12 -k menlo --no-trailing-nl | pbcopy
 }
