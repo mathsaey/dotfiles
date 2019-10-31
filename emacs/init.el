8fe5f5b820676ee3a17407857d29100305948723
Mathijs Saey <mathsaey@vub.ac.be>
2019-10-31 16:53:21 +0100

    Work on emacs configuration for a bit

--- emacs/init.el
+++ emacs/init.el
@@ -1,2 +1,2 @@
 ; For documentation purposes we write our configuration in org.
-(org-babel-load-file (expand-file-name "conf.org" user-emacs-directory))
+(org-babel-load-file (expand-file-name "conf/init.org" user-emacs-directory))
