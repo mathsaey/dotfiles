ac7f9608e10fd6c468e8bf4e827a70df75e3cfaa
Mathijs Saey <mathijs@mathsaey.be>
2025-04-26 03:07:08 +0200

    Use --no-wait to prevent blocking

--- waybar/update_world.sh
+++ waybar/update_world.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-footclient --hold \
+footclient --hold --no-wait \
   powerprofilesctl launch --reason "Updating @world" --profile performance \
   sudo emerge --update --changed-use --deep --with-bdeps=y --keep-going @world
