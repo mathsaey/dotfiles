591c935cb67c47a496a2e77d4326b9e6a6b860b3
Mathijs Saey <mathijs@mathsaey.be>
2025-11-25 00:47:16 +0100

    Prevent idle while updating

--- waybar/update_world.sh
+++ waybar/update_world.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
 footclient --hold --no-wait \
+  systemd-inhibit --who="portage" --why="Don't sleep while updating" \
   powerprofilesctl launch --reason "Updating @world" --profile performance \
   sudo emerge --update --changed-use --deep --with-bdeps=y --keep-going @world
